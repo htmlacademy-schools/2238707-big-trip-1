3580337553d67d5ce3e3a065f1c3e5bc3d3d0907
bluzayz <114312004+bluzayz@users.noreply.github.com>
2022-10-05 21:10:09 +0300

    Update events-list-view.js

--- src/view/events-list-view.js
+++ src/view/events-list-view.js
@@ -1,5 +1,10 @@
-export const createEventsListTemplate = () => (
-  `<ul class="trip-events__list">
-      </ul>`
-);
+import AbstractView from './abstract-view';
+
+const createEventsListTemplate = () => ('<ul class="trip-events__list"></ul>');
+
+export default class EventsListView extends AbstractView {
+  get template() {
+    return createEventsListTemplate();
+  }
+}
 

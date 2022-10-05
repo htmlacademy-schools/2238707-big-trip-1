56510c3abba9130acb775089abfc676b1f2f508f
bluzayz <114312004+bluzayz@users.noreply.github.com>
2022-10-05 22:37:21 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,75 +1,22 @@
-import {render, RenderPosition} from './render.js';
-import EventAddView from './view/event-add-view';
+import { render, RenderPosition } from './render.js';
 import TripFiltersView from './view/trip-filters-view.js';
 import TripInfoView from './view/trip-info-view.js';
-import TripSortView from './view/trip-sort-view.js';
 import TripTabsView from './view/trip-tabs-view.js';
-import EventEditView from './view/event-edit-view';
-import EventsListView from './view/events-list-view.js';
-import WaypointView from './view/waypoint-view.js';
 import {generatePoint} from './mock/point.js';
-import NoTripPointView from './view/no-trip-point-view';
+import TripPresenter from './presenter/trip-presenter';
 
 const POINT_COUNT = 14;
 
 const points = Array.from({length: POINT_COUNT}, generatePoint);
+const pageMainElement = document.querySelector('.page-body');
 
 const siteTripMainElement = document.querySelector('.trip-main');
 const tripControlsNavigationElement = document.querySelector('.trip-controls__navigation');
 const tripControlsFiltersElement = document.querySelector('.trip-controls__filters');
-const tripEventsElement = document.querySelector('.trip-events');
-const tripEventsListElement = new EventsListView();
 
-//render(siteTripMainElement, new TripInfoView().element, RenderPosition.AFTERBEGIN);
+render(siteTripMainElement, new TripInfoView().element, RenderPosition.AFTERBEGIN);
 render(tripControlsNavigationElement, new TripTabsView(), RenderPosition.BEFOREEND);
 render(tripControlsFiltersElement, new TripFiltersView(), RenderPosition.BEFOREEND);
 
-if (points.length === 0) {
-  render(tripEventsElement, new NoTripPointView(), RenderPosition.BEFOREEND);
-} else {
-  render(siteTripMainElement, new TripInfoView(), RenderPosition.AFTERBEGIN);
-  render(tripEventsElement, tripEventsListElement, RenderPosition.BEFOREEND);
-  render(tripEventsElement, new TripSortView(), RenderPosition.AFTERBEGIN);
-  render(tripEventsListElement.element, new EventAddView(points[0]), RenderPosition.BEFOREEND);
-}
-
-const renderPoint = (pointListElement, point) => {
-  const waypointComponent = new WaypointView(point);
-  const eventEditComponent = new EventEditView(point);
-
-  const replaceItemToForm = () => {
-    pointListElement.replaceChild(eventEditComponent.element, waypointComponent.element);
-  };
-  const replaceFormToItem = () => {
-    pointListElement.replaceChild(waypointComponent.element, eventEditComponent.element);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.key === 'Escape' || evt.key === 'Esc') {
-      evt.preventDefault();
-      replaceFormToItem();
-      document.removeEventListener('keydown', onEscKeyDown);
-    }
-  };
-
-  waypointComponent.setEditClickHandler(() => {
-    replaceItemToForm();
-    document.addEventListener('keydown', onEscKeyDown);
-  });
-
-  eventEditComponent.setRollupClickHandler(() => {
-    replaceFormToItem();
-    document.addEventListener('keydown', onEscKeyDown);
-  });
-
-  eventEditComponent.setFormSubmit(() => {
-    replaceFormToItem();
-    document.removeEventListener('keydown', onEscKeyDown);
-  });
-
-  render(pointListElement, waypointComponent, RenderPosition.BEFOREEND);
-};
-
-for (let i = 1; i < POINT_COUNT; i++) {
-  renderPoint(tripEventsListElement.element, points[i]);
-}
+const tripPresenter = new TripPresenter(pageMainElement);
+tripPresenter.init(points);
